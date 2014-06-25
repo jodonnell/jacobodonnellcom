7ede7b4d68cdf9f5c9f78132e64fd2b1a2d9c6fb
Jacob O'Donnell <jacobodonnell@gmail.com>
2014-06-25 16:59:58 -0400

    code after creating final movie

--- video_code/consultant.js
+++ video_code/consultant.js
@@ -1,11 +1,34 @@
 "use strict";
 
-function Consultant(name) {
+var _ = require('lodash');
+
+function Consultant(name, projects) {
 		this.name = name;
+    this.projects = projects;
 }
 
 Consultant.prototype = {
-    
+    firstProject: function () {
+				return _.first(this.projects);
+    },
+
+    lastProject: function () {
+				return _.last(this.projects);
+    },
+
+    middleProject: function () {
+				return this.projects[Math.floor(this.projects.length / 2)];
+    },
+
+    addProject: function (newProject) {
+				this.projects.push(newProject);
+    },
+
+    removeProject: function (removeProject) {
+        _.remove(this.projects, function(element) {return element == removeProject});
+    }
+
+
 };
 
 module.exports = Consultant;
--- video_code/test/consultant_test.js
+++ video_code/test/consultant_test.js
@@ -6,8 +6,40 @@ var expect = require('expect.js');
 var Consultant = require('../consultant');
 
 describe('Consultant', function(){
-    it ('can function', function(){
-        var consultant = new Consultant('Jacob');
+    var consultant;
+    
+    beforeEach(function(){
+        consultant = new Consultant('Jacob', ['Forrest Avenue', 'Time tracking', 'Jumping Rope']);
+    });
+    
+    it ('has a name', function(){
         expect(consultant.name).to.be('Jacob');
     });
+
+    it('has projects', function () {
+				expect(consultant.projects).to.eql(['Forrest Avenue', 'Time tracking', 'Jumping Rope']);
+    });
+
+    it('can get the first project', function () {
+				expect(consultant.firstProject()).to.be('Forrest Avenue');
+    });
+
+    it('can get the last project', function () {
+				expect(consultant.lastProject()).to.be('Jumping Rope');
+    });
+
+    it('can get the middle project', function () {
+				expect(consultant.middleProject()).to.be('Time tracking');
+    });
+
+    it('can add a new project', function () {
+        consultant.addProject('Big boom');
+				expect(consultant.lastProject()).to.be('Big boom');
+    });
+    
+    it('can remove a project', function () {
+        consultant.removeProject('Jumping Rope');
+				expect(consultant.lastProject()).to.be('Time tracking');
+    });
+    
 });
